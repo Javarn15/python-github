a4fab831b1f4314e78ed6e109e6a59450a1f53e1
Javarn <javarnray10@gmail.com>
2025-03-21 12:37:37 +1300

    added len

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-print("helllo")
-print("hi")
+name_list = ["jack" , "jim" , "jason"]
+age_list = [14,16,15]
+list_count = 0
+while list_count < len(name_list):
+    print(f"the name is {name_list[list_count]} and the age is {age_list[list_count]}")
+    list_count +=1
